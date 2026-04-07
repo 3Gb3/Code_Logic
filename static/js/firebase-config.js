92068b5c78c84661aeec86313afbdcb8723789f6
Gabriel Schwingel Conci <gabrielbrawlstars717@gmail.com>
2026-04-07 14:13:48 -0300

    fix: requirements

--- static/js/firebase-config.js
+++ static/js/firebase-config.js
@@ -3,8 +3,9 @@
 const firebaseConfig = {
   apiKey: "AIzaSyBpELaOaoDI-4SOpRpdNJ3AiDIRkDFd9FE",
   authDomain: "codelogic-f9f59.firebaseapp.com",
+  databaseURL: "https://codelogic-f9f59-default-rtdb.firebaseio.com",
   projectId: "codelogic-f9f59",
-  storageBucket: "codelogic-f9f59.appspot.com",
+  storageBucket: "codelogic-f9f59.firebasestorage.app",
   messagingSenderId: "510220155196",
   appId: "1:510220155196:web:64608af448ffc8f94c0639",
   measurementId: "G-52CS4P28XZ"
